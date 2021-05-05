1703f1f03e208a3286667a3d999112661102d565
chi.nguyen@s3corp.com.vn <chi.nguyen@s3corp.com.vn>
2021-05-05 15:24:34 +0700

    search img
    (cherry picked from commit 06a659cb71a71adb794082fea79501389d857371)

---
+++ ResearchImg/myfirstreact/src/Components/imageresult/ImageResult.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import ProTypes from 'prop-types';
+import { GridList, GridTile } from 'material-ui/GridList'
+import IconButton from 'material-ui/IconButton'
+import ZoomIn from 'material-ui/svg-icons/action/zoom-in'
+import Dialog from 'material-ui/Dialog'
+import FlatButton from 'material-ui/FlatButton'
+
+class ImageResults extends Component {
+    render() {
+
+        let imageList;
+        const { images } = this.props;
+
+        if (images) {
+            imageList = (
+                <GridList cols={4}>
+                    {
+                        images.map(img => (
+                            <GridTile
+                                title={img.tags}
+                                key={img.id}
+                                actionIcon={
+                                    <IconButton>
+                                        <ZoomIn color="white" />
+                                    </IconButton>
+                                }
+                            >
+                                <img src={img.largeImageURL} alt="" />
+                            </GridTile>
+
+                        ))
+                    }
+
+                </GridList>
+            )
+
+        }
+
+        else {
+            imageList = null;
+
+        }
+        return (
+            <div>
+                {imageList}
+
+            </div>
+        )
+
+
+
+    }
+
+
+}
+ImageResults.protypes = {
+    images: ProTypes.array.isRequired
+
+}
+export default ImageResults;
---
+++ ResearchImg/myfirstreact/src/Components/search/Search.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+import ImageResults from "../imageresult/ImageResult";
+class Search extends Component {
+    state = {
+        searchText: '',
+        apiUrl: 'http://pixabay.com/api',
+        apiKey: '21472884-19e587673705284b460076df6',
+        images: []
+    };
+    onTextChange = e => {
+        this.setState({ [e.target.name]: e.target.value }, () => {
+            axios
+                .get(
+                    `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText
+                    }&image_type=photo&safesearch=true`
+                )
+                .then(res => this.setState({ images: res.data.hits }))
+                .catch(err => console.log(err));
+        });
+    }
+
+    render() {
+        console.log(this.state.images)
+        return (
+            <div>
+                <input type="text"
+                    style={
+                        {
+                            backgroundColor: 'black',
+                            color: 'white',
+                            marginLeft: 570,
+                            marginTop: 100,
+                            paddingTop: 100,
+                            paddingLeft: 70,
+                            fontSize: 30,
+                            borderTopStyle: "hidden",
+                            borderRightStyle: "hidden",
+                            borderTopStyle: "hidden",
+                            outline: "none",
+                            borderBottomStyle: "groove",
+
+                        }
+
+                    }
+                    placeholder="search image"
+                    name="searchText"
+                    value={this.state.searchText}
+                    onChange={this.onTextChange}></input>
+                {this.state.images.length > 0 ? (<ImageResults images={this.state.images} />) : null}
+            </div>
+        )
+    }
+}
+export default Search
